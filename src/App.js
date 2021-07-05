e65c4aba68de415fe51a31579ac529002ccf5657
Glenn Pinto <gvpinto@gmail.com>
2021-07-05 16:16:30 -0400

    60. conditionally rendering content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
+  }
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({
+          errorMessage: err.message,
+        });
+      }
     );
-    return <div>Latitude:</div>;
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
